55eec53d4af7e7cfc482669c66009f36e3ffecd9
shoray2002 <shoryasinghall@gmail.com>
2023-04-21 23:14:19 +0530

    big chungus commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/LandingPage_comp/Home";
-import About from "./Components/LandingPage_comp/About";
-import Testimonials from "./Components/LandingPage_comp/Testimonials";
-import Contact from "./Components/LandingPage_comp/Contact";
+// import Home from "./Components/LandingPage_comp/Home";
+// import About from "./Components/LandingPage_comp/About";
+// import Testimonials from "./Components/LandingPage_comp/Testimonials";
+// import Contact from "./Components/LandingPage_comp/Contact";
 import { LandingPage } from "./pages/LandingPage/landing_page";
+import { Home } from "./pages/Home/home";
 import { HomePage } from "./pages/HomePage/home_page";
 import SignUpForm from "./pages/SignUpPage/sign-up";
 import PhoneAuth from "./pages/PhoneAuthentication/PhoneAuth";
@@ -13,7 +14,6 @@ import CreatePost from "./pages/CreatePost/create-post";
 import { PhoneNumberProvider } from "./Context/PhoneNumberContext";
 // onClick={() => navigate('/home')} to navigate to that particular route
 function App() {
-  
   return (
     <div className="App">
       {/* <ShopContextProvider> */}
@@ -24,7 +24,7 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             {/* <Route path="/home" element={<Contact />} />
             <Route path="/cart" element={<Cart />} /> */}
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/signup" element={<SignUpForm />} />
             <Route path="/phone-auth" element={<PhoneAuth />} />
             <Route path="/login" element={<LoginPage />} />
--- src/pages/HomePage/home_page.jsx
+++ src/pages/HomePage/home_page.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import './home_page.css'
-import {appWrapper, contentWrapper, sideBarWrapper, scrollListWrapper, contentAreaWrapper} from '../../Components/HomePage_comp/styles/styles'
-import { Box } from '@mui/system';
-import Navbar from '../../Components/HomePage_comp/navigation/Navbar'
-import SideList from '../../Components/HomePage_comp/SideList'
-import AppContentArea from '../../Components/HomePage_comp/AppContentArea'
+import React from "react";
+import "./home_page.css";
+import {
+  appWrapper,
+  contentWrapper,
+  sideBarWrapper,
+  scrollListWrapper,
+  contentAreaWrapper,
+} from "../../Components/HomePage_comp/styles/styles";
+import { Box } from "@mui/system";
+import Navbar from "../../Components/HomePage_comp/navigation/Navbar";
+import SideList from "../../Components/HomePage_comp/SideList";
+import AppContentArea from "../../Components/HomePage_comp/AppContentArea";
 export const HomePage = () => {
-    return (
-        <React.Fragment>
-          <Box sx={appWrapper}>
-            <Navbar />
-            <Box sx={contentWrapper}>
-              <Box sx={sideBarWrapper}>
-                <Box sx={scrollListWrapper}>
-                  <SideList />
-                </Box>
-            </Box>
-              <Box sx={contentAreaWrapper}>
-                <AppContentArea />
-              </Box>
+  return (
+    <React.Fragment>
+      <Box sx={appWrapper}>
+        <Navbar />
+        <Box sx={contentWrapper}>
+          <Box sx={sideBarWrapper}>
+            <Box sx={scrollListWrapper}>
+              <SideList />
             </Box>
           </Box>
-        </React.Fragment>
-      );
-}
+          <Box sx={contentAreaWrapper}>
+            <AppContentArea />
+          </Box>
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
+};
