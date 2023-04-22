c1dbc709dc5a04c97b5f49bd2dacf002fd4aa485
Namgyal Angmo Shakspo <angmo@pop-os.localdomain>
2023-04-23 05:21:19 +0530

    added join communities page, redirecting to commuites to be added

---
+++ src/Components/communityHome/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h1>Join your community!</h1>
+    <h2>
+      <span>“</span> Discover the hidden gems in your community  <span>”</span>
+    </h2>
+    <p>
+    Empowering local businesses,  <br /> one transaction at a time
+    </p>
+  </header>
+);
+
+export default Header;
