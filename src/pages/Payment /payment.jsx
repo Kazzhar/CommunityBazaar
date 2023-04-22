19e6dcfd944a64e8bcee5465615a84c5c4725ea4
Namgyal Angmo Shakspo <angmo@pop-os.localdomain>
2023-04-22 19:29:22 +0530

    payment begun

---
+++ src/pages/Payment /payment.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './payment.css';
+
+function App() {
+    return (
+        <div ClassName="App">
+            <h1>Payment Getway</h1>
+            <hr />
+        </div>
+
+    );
+}
+export default App;
